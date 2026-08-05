a9a861ee83b64b574755ed15a9f31466170b55d8
Mohammad Mustafa <ff774823@gmail.com>
2026-08-05 23:30:54 +0530

    Fix import paths for PlatformSection and FeatureSection
    Corrected import statements for PlatformSection and FeatureSection.

--- client/unifetch-media/src/Pages/HeroPageCTA.jsx
+++ client/unifetch-media/src/Pages/HeroPageCTA.jsx
@@ -2,8 +2,8 @@ import { useRef } from "react";
 
 import NavBar from "../Components/HeroSectionCTA/NavBar";
 import HeroSection from "../Components/HeroSectionCTA/HeroSection";
-import PlatformSection from "../Components/HeroSectionCTA/PlatformSection";
-import FeatureSection from "../Components/HeroSectionCTA/FeatureSection";
+import PlatformSection from "../Components/HeroSectionCTA/PlatformSectio";
+import FeatureSection from "../Components/HeroSectionCTA/Featuresection";
 import HowItsWork from "../Components/HeroSectionCTA/HowitsWork";
 import DashboardShowcase from "../Components/HeroSectionCTA/DashBoard/DashBoardShowcase";
 import TrustSection from "../Components/HeroSectionCTA/TrustSection";
