fcc9539bddcefdc031657e61c8b42d46e2e4d33c
MohammadMustafa23 <ff774823@gmail.com>
2026-08-06 18:45:58 +0530

    adding logs For error

--- server/src/Feature/download/controller/dashboard.controller.js
+++ server/src/Feature/download/controller/dashboard.controller.js
@@ -128,6 +128,9 @@ export const getDashboard = async (req, res) => {
       ex: 60 * 2,
     });
 
+    console.log(dashboardData);
+    
+
     return res.status(200).json({
       success: true,
       data: dashboardData,
