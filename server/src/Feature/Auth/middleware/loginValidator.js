9d3375011b668675e4725647a9013a6db10ed44e
Mohammad Mustafa <ff774823@gmail.com>
2026-09-07 21:38:00 +0530

    Fix email validation message formatting

--- server/src/Feature/Auth/middleware/loginValidator.js
+++ server/src/Feature/Auth/middleware/loginValidator.js
@@ -7,8 +7,7 @@ const validateLogin = [
     .withMessage("Email is required.")
     .bail()
     .isEmail()
-    .withMessage("Please enter a valid email address.")
-    .normalizeEmail(),
+    .withMessage("Please enter a valid email address."),
 
   body("password")
     .notEmpty()
